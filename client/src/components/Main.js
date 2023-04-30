91e7eceea29028102c5b4dc74c8486c127ea1b6d
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-04-30 16:43:58 +0545

    minor fix

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import '../App.css';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Main() {
   const [value, setValue] = React.useState([])
@@ -29,26 +27,17 @@ function Main() {
       console.error('Error deleting data:', error);
     }
   }
-  const editData = async (id) => {
-    try {
-      await axios.put(`http://localhost:8000/api/${id}`)
-    }catch (e){
-      console.error('Error while editing the data',e);
-    }
-  }
+  // const editData = async (id) => {
+  //   try {
+  //     await axios.put(`http://localhost:8000/api/${id}`)
+  //   }catch (e){
+  //     console.error('Error while editing the')
+  //   }
+  // }
 
 
   return (
     <div className='main'>
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
-
-      <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-          <div class="modal-content">
-            Your text here
-          </div>
-        </div>
-    </div>
       <div className="table">
         <table>
           <thead>
@@ -68,7 +57,7 @@ function Main() {
                 <td>{user.email}</td>
                 <td>{user.gender}</td>
                 <td><button onClick={()=>deleteData(user._id)}>Del</button></td>
-                <td><button onClick={()=>editData(user._id)} data-toggle="modal" data-target=".bd-example-modal-sm">Edit</button></td>
+                <td><button>Edit</button></td>
               </tr>
             ))}
           </tbody>
